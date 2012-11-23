514a3c4c12b1d79ee03ff620e3855fba4cae4729
Bruce Hubbard <brucehubbard@gmail.com>
2012-11-23 17:34:11 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisualCraigslist::Application.config.secret_token = '7437d7f63aead93ccc3e344959dc85231a59e01847124ae13f530f2e7cfdb00dbbf785d267e82ab6300dd1de7d2be40d86485a7be8560e6bddf7b247c3073445'
