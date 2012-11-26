4b5f1cd56f6676a601cc28381fac820605978602
Bruce Hubbard <brucehubbard@gmail.com>
2012-11-26 08:41:51 -0500

    Improving pic puller code

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,7 +9,8 @@ class Listing
 	def self.search (search_term)
 		listings = []
 	    doc = Nokogiri::HTML.parse(open(@@search_url))
-	    for listing_html in doc.css('p.row')
+
+	    for listing_html in doc.css('p.row').take(10)
 	    	listing = Listing.new
 	    	listing.date = listing_html.css('.itemdate')[0].content.strip
 	    	listing.title = listing_html.css('a')[0].content.strip
@@ -20,32 +21,13 @@ class Listing
 
 	    	listings << listing
 	    end
-#	    listings = ButlerListingParser.new(doc.to_s).listings
-#	    all_auctions = []
-#	    
-#	    for listing in listings do
-#	      url = @@urlBase + listing.link
-#	      all_auctions.concat(ButlerAuctionParser.new(Net::HTTP.get(URI.parse(url))).auctions)
-#	    end
-	    
-#	    all_auctions		
-#	    for link in @html.css('#AutoNumber1 a') do
-#	      list << Listing.new(link["href"], link.content.gsub(/\n\t/, "").gsub(/\s+/, " ").strip)
-#	    end
+
 		listings
 	end
 
 	private
 
 	def self.pics (url)
-		pics = []
-
-		detail_doc = Nokogiri::HTML.parse(open(url))
-
-		for img in detail_doc.css('.tn a')
-			pics << img["href"]
-		end
-
-		pics
+		Nokogiri::HTML.parse(open(url)).css('.tn a').collect {|img| img["href"]}
 	end
 end
