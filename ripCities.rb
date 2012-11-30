da70c4c24654c7ead376ea147923b39802fa762e
Bruce Hubbard <brucehubbard@gmail.com>
2012-11-30 15:51:53 -0500

    Ripping all US cities

---
+++ ripCities.rb
@@ -0,0 +1,84 @@
+require 'nokogiri'
+
+state_abbr = {
+  'AL' => 'Alabama',
+  'AK' => 'Alaska',
+  'AS' => 'America Samoa',
+  'AZ' => 'Arizona',
+  'AR' => 'Arkansas',
+  'CA' => 'California',
+  'CO' => 'Colorado',
+  'CT' => 'Connecticut',
+  'DE' => 'Delaware',
+  'DC' => 'District of Columbia',
+  'FM' => 'Micronesia1',
+  'FL' => 'Florida',
+  'GA' => 'Georgia',
+  'GU' => 'Guam',
+  'HI' => 'Hawaii',
+  'ID' => 'Idaho',
+  'IL' => 'Illinois',
+  'IN' => 'Indiana',
+  'IA' => 'Iowa',
+  'KS' => 'Kansas',
+  'KY' => 'Kentucky',
+  'LA' => 'Louisiana',
+  'ME' => 'Maine',
+  'MH' => 'Islands1',
+  'MD' => 'Maryland',
+  'MA' => 'Massachusetts',
+  'MI' => 'Michigan',
+  'MN' => 'Minnesota',
+  'MS' => 'Mississippi',
+  'MO' => 'Missouri',
+  'MT' => 'Montana',
+  'NE' => 'Nebraska',
+  'NV' => 'Nevada',
+  'NH' => 'New Hampshire',
+  'NJ' => 'New Jersey',
+  'NM' => 'New Mexico',
+  'NY' => 'New York',
+  'NC' => 'North Carolina',
+  'ND' => 'North Dakota',
+  'OH' => 'Ohio',
+  'OK' => 'Oklahoma',
+  'OR' => 'Oregon',
+  'PW' => 'Palau',
+  'PA' => 'Pennsylvania',
+  'PR' => 'Puerto Rico',
+  'RI' => 'Rhode Island',
+  'SC' => 'South Carolina',
+  'SD' => 'South Dakota',
+  'TN' => 'Tennessee',
+  'TX' => 'Texas',
+  'UT' => 'Utah',
+  'VT' => 'Vermont',
+  'VI' => 'Virgin Island',
+  'VA' => 'Virginia',
+  'WA' => 'Washington',
+  'WV' => 'West Virginia',
+  'WI' => 'Wisconsin',
+  'WY' => 'Wyoming'
+}
+
+doc = Nokogiri::HTML.parse(File.open('cities.html'))
+
+us = doc.css('.continent_header a[@name = "US"]').first.parent.parent
+
+for state in us.css('.state_delimiter')
+	name = state.content()
+
+	abbr = state_abbr.select{|key, val| val.downcase == name.downcase }
+
+	if(abbr.length > 0)
+		a = abbr.first[0]
+		cities = state.next_element.css('a')
+
+		for city in cities
+			city_name = city.content()
+			city_url = city["href"].gsub('.craigslist.org', '').gsub('http://', '')
+			puts "<option value=\"#{city_url}\">#{a} - #{city_name}</option>"
+		end
+	end
+end
+
